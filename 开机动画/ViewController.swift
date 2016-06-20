6b8ca6a6488705860146f2e7e4bbabd26e7c2963
聚石慧 <JuShiHui178@163.com>
2016-06-20 10:19:48 +0800

    Initial Commit

---
+++ 开机动画/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  开机动画
+//
+//  Created by 聚石慧 on 16/6/20.
+//  Copyright © 2016年 开机动画. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
